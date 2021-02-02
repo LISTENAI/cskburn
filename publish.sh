b8d260d5b8ad3009de20343b6d3719670db1f10c
陈星宇 <xychen@listenai.com>
2021-02-02 22:26:40 +0800

    Add darwin build

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "${LPMRC}" > ~/.npmrc
 mkdir .publish
-cp README.md package.json cskburn-win32.exe cskburn-linux .publish
+cp README.md package.json cskburn-win32.exe cskburn-linux cskburn-darwin .publish
 cd .publish
 npm --registry "${LPM_REGISTRY}" publish
