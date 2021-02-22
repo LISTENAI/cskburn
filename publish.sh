fef9512fda31954943727e3060e2563cca79a279
陈星宇 <xychen@listenai.com>
2021-02-22 20:20:13 +0800

    Move NPM stuffs to npm/

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "${LPMRC}" > ~/.npmrc
-mkdir .publish
-cp README.md package.json cskburn-win32.exe cskburn-linux cskburn-darwin .publish
-cd .publish
+cp README.md cskburn-win32.exe cskburn-linux cskburn-darwin npm/
+cd npm
 npm --registry "${LPM_REGISTRY}" publish
