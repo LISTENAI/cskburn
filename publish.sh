a78a4a2a6454cdb23b5f6319bf11b44c385c5796
陈星宇 <xychen@listenai.com>
2021-02-24 18:03:19 +0800

    Adjust script for publishing to NPM

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo "${LPMRC}" > ~/.npmrc
+echo "${NPMRC}" > ~/.npmrc
 cp README.md cskburn-win32.exe cskburn-linux cskburn-darwin npm/
+cp package_template.json npm/package.json
 cd npm
-npm --registry "${LPM_REGISTRY}" publish
+sed -i "s|{{NAME}}|${PKG_NAME}|" package.json
+sed -i "s|{{VERSION}}|${PKG_VERSION}|" package.json
+npm publish --access public
