cd0654c42261b1dc71d40df5b1239f99ec95d03e
陈星宇 <xychen@listenai.com>
2021-05-17 15:27:24 +0800

    Add support for Alpine Linux

--- npm/index.js
+++ npm/index.js
@@ -1,10 +1,12 @@
 const { join } = require('path');
 const { existsSync } = require('fs');
+const { family, MUSL } = require('detect-libc');
 const { arch, platform } = process;
 
 exports.CSKBURN = (() => {
     const suffix = (platform == 'win32') ? '.exe' : '';
-    const binary = join(__dirname, `cskburn-${platform}-${arch}${suffix}`);
+    const libc = (platform == 'linux' && family == MUSL) ? '-musl' : '';
+    const binary = join(__dirname, `cskburn-${platform}-${arch}${libc}${suffix}`);
     if (existsSync(binary)) {
         return binary;
     } else {
--- publish.sh
+++ publish.sh
@@ -3,7 +3,9 @@ echo "${NPMRC}" > ~/.npmrc
 cp README.md npm/
 cp cskburn-win32-x64.exe npm/
 cp cskburn-linux-x64 npm/
+cp cskburn-linux-x64-musl npm/
 cp cskburn-linux-arm npm/
+cp cskburn-linux-arm-musl npm/
 cp package_template.json npm/package.json
 cd npm
 sed -i "s|{{NAME}}|${PKG_NAME}|" package.json
