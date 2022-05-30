7836eaf230cea2a627ac02f50309e7ea7446c350
xychen <xychen@listenai.com>
2022-05-30 14:56:12 +0800

    serial: win32: 串口读写增加超时处理

--- libserial/src/serial_win32.c
+++ libserial/src/serial_win32.c
@@ -1,9 +1,10 @@
 #include <serial.h>
-
 #include <windows.h>
 
 struct _serial_dev_t {
 	HANDLE handle;
+	OVERLAPPED overlapped_read;
+	OVERLAPPED overlapped_write;
 };
 
 serial_dev_t *
@@ -14,7 +15,7 @@ serial_open(const char *path)
 			0,  // No Sharing
 			NULL,  // No Security
 			OPEN_EXISTING,  // Open existing port only
-			0,  // Non Overlapped I/O
+			FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,  // Overlapped I/O
 			NULL);  // Null for Comm Devices
 
 	if (handle == INVALID_HANDLE_VALUE) {
@@ -54,12 +55,20 @@ serial_open(const char *path)
 	serial_dev_t *dev = (serial_dev_t *)malloc(sizeof(serial_dev_t));
 	dev->handle = handle;
 
+	SecureZeroMemory(&dev->overlapped_read, sizeof(OVERLAPPED));
+	dev->overlapped_read.hEvent = CreateEventA(NULL, TRUE, FALSE, NULL);
+
+	SecureZeroMemory(&dev->overlapped_write, sizeof(OVERLAPPED));
+	dev->overlapped_write.hEvent = CreateEventA(NULL, FALSE, FALSE, NULL);
+
 	return dev;
 }
 
 void
 serial_close(serial_dev_t **dev)
 {
+	CloseHandle((*dev)->overlapped_read.hEvent);
+	CloseHandle((*dev)->overlapped_write.hEvent);
 	CloseHandle((*dev)->handle);
 	free(*dev);
 	*dev = NULL;
@@ -85,26 +94,48 @@ serial_set_speed(serial_dev_t *dev, uint32_t speed)
 	return true;
 }
 
+static bool
+handle_overlapped_result(HANDLE handle, LPOVERLAPPED overlapped, LPDWORD count, uint64_t timeout)
+{
+	if (GetLastError() != ERROR_IO_PENDING) {
+		return false;
+	}
+
+	if (WaitForSingleObject(overlapped->hEvent, timeout) == WAIT_OBJECT_0) {
+		return GetOverlappedResult(handle, overlapped, count, FALSE);
+	}
+
+	return false;
+}
+
 int32_t
 serial_read(serial_dev_t *dev, void *buf, size_t count, uint64_t timeout)
 {
-	size_t read = 0;
-	if (ReadFile(dev->handle, buf, count, &read, NULL)) {
-		return read;
-	} else {
-		return -1;
+	DWORD read = 0;
+
+	ResetEvent(dev->overlapped_read.hEvent);
+
+	if (ReadFile(dev->handle, buf, count, (LPDWORD)&read, &dev->overlapped_read) == FALSE) {
+		if (!handle_overlapped_result(dev->handle, &dev->overlapped_read, &read, timeout)) {
+			return -1;
+		}
 	}
+
+	return read;
 }
 
 int32_t
 serial_write(serial_dev_t *dev, const void *buf, size_t count, uint64_t timeout)
 {
-	size_t wrote = 0;
-	if (WriteFile(dev->handle, buf, count, &wrote, NULL)) {
-		return wrote;
-	} else {
-		return -1;
+	DWORD wrote = 0;
+
+	if (WriteFile(dev->handle, buf, count, (LPDWORD)&wrote, &dev->overlapped_write) == FALSE) {
+		if (!handle_overlapped_result(dev->handle, &dev->overlapped_write, &wrote, timeout)) {
+			return -1;
+		}
 	}
+
+	return wrote;
 }
 
 void
