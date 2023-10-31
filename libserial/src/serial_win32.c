71e732913a6550514f9f893127e7583116812a25
xychen <xychen@listenai.com>
2023-10-31 12:01:07 +0800

    serial: 优化 win32 串口读取延迟

--- libserial/src/serial_win32.c
+++ libserial/src/serial_win32.c
@@ -135,6 +135,39 @@ serial_read(serial_dev_t *dev, void *buf, size_t count, uint64_t timeout)
 {
 	DWORD read = 0;
 
+	// Wait for the first byte to be available
+
+	ResetEvent(dev->overlapped_read.hEvent);
+
+	if (ReadFile(dev->handle, buf, 1, (LPDWORD)&read, &dev->overlapped_read) == FALSE) {
+		if (!handle_overlapped_result(dev->handle, &dev->overlapped_read, &read, timeout)) {
+			return -1;
+		}
+	}
+
+	if (read == 0) {
+		return 0;
+	}
+
+	// Get number of remaining bytes
+
+	DWORD errors;
+	COMSTAT stat;
+	if (ClearCommError(dev->handle, &errors, &stat) == 0) {
+		return -1;
+	}
+
+	if (stat.cbInQue == 0) {
+		return read;
+	}
+
+	buf = (uint8_t *)buf + 1;
+	count -= 1;
+
+	count = count < stat.cbInQue ? count : stat.cbInQue;
+
+	// Read the remaining bytes
+
 	ResetEvent(dev->overlapped_read.hEvent);
 
 	if (ReadFile(dev->handle, buf, count, (LPDWORD)&read, &dev->overlapped_read) == FALSE) {
@@ -143,13 +176,15 @@ serial_read(serial_dev_t *dev, void *buf, size_t count, uint64_t timeout)
 		}
 	}
 
-	return read;
+	return read + 1;
 }
 
 int32_t
 serial_write(serial_dev_t *dev, const void *buf, size_t count, uint64_t timeout)
 {
 	DWORD wrote = 0;
+
+	ResetEvent(dev->overlapped_write.hEvent);
 
 	if (WriteFile(dev->handle, buf, count, (LPDWORD)&wrote, &dev->overlapped_write) == FALSE) {
 		if (!handle_overlapped_result(dev->handle, &dev->overlapped_write, &wrote, timeout)) {
